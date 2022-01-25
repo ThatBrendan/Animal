913a6cd246f767cc89a9af03c3a3ed96a53e03d8
Brendan <macintosh@macintoshs-mbp.home>
2022-01-25 10:15:34 +0000

    cleaned up code further by adjusting main.java file

--- src/main/java/com/Animal/Main.java
+++ src/main/java/com/Animal/Main.java
@@ -3,15 +3,5 @@ package com.Animal;
 import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args){
-        Dog dog = new Dog();
-        ArrayList<Animal>pound = new ArrayList<>();
-        pound.add(dog);
-
-        for (Animal a: pound){
-            a.eat("Bones");
-            System.out.println(a.Breathe());
-            a.reproduce();
-        }
-    }
+    
 }
